5f34490dbdfb8da13863dbbc4566a7410dc6315b
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-05-31 03:12:38 +0700

    update history.py

--- Naya/modules/history.py
+++ Naya/modules/history.py
@@ -7,7 +7,6 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 
 from Naya.utils.dbfunctions import *
-from Naya import app
 from Naya.core.decorator.permissions import adminsOnly
 from Naya import (
     app,
