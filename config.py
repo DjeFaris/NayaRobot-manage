e40aa12e211ffdec7e880b0e99d4c4421750fd4a
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-05-30 21:47:19 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,24 @@
+
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = getenv('BOT_TOKEN')
+API_ID = int(getenv('API_ID'))
+SESSION = getenv('SESSION', '')
+OPENAI_API = getenv('OPENAI_API', '')
+API_HASH = getenv('API_HASH')
+USERBOT_PREFIX = getenv('USERBOT_PREFIX', '.')
+PHONE_NUMBER = getenv('PHONE_NUMBER')
+SUDO_USERS_ID = list(map(int, getenv('SUDO_USERS_ID', '').split()))
+LOG_GROUP_ID = int(getenv('LOG_GROUP_ID'))
+GBAN_LOG_GROUP_ID = int(getenv('GBAN_LOG_GROUP_ID'))
+MESSAGE_DUMP_CHAT = int(getenv('MESSAGE_DUMP_CHAT'))
+WELCOME_DELAY_KICK_SEC = int(getenv('WELCOME_DELAY_KICK_SEC', 600))
+MONGO_URL = getenv('MONGO_URL')
+ARQ_API_KEY = getenv('ARQ_API_KEY')
+ARQ_API_URL = getenv('ARQ_API_URL', 'https://arq.hamker.in')
+LOG_MENTIONS = getenv('LOG_MENTIONS', 'True').lower() in ['true', '1']
+RSS_DELAY = int(getenv('RSS_DELAY', 300))
+PM_PERMIT = getenv('PM_PERMIT', 'True').lower() in ['true', '1']
