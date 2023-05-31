780f7c1444516548e746579e62c209630bb11420
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-05-31 17:06:17 +0700

    update misc.py

--- Naya/modules/misc.py
+++ Naya/modules/misc.py
@@ -10,12 +10,21 @@ import subprocess
 from asyncio import Lock
 from re import findall
 
-from pyrogram import enums, filters
-
-from Naya import SUDOERS, USERBOT_PREFIX, app, app2, arq, eor
+import json
+import os
+import asyncio
+import traceback
+from PIL import Image
+from logging import getLogger
+from urllib.parse import quote
+import aiohttp
+from bs4 import BeautifulSoup
+from pyrogram import *
+from pyrogram.types import *
+from Naya import *
 from Naya.core.decorators.errors import capture_err
 from Naya.utils import random_line
-from Naya.utils.http import get
+from Naya.utils.http import get, http
 from Naya.utils.json_prettify import json_prettify
 from Naya.utils.pastebin import paste
 
@@ -25,7 +34,12 @@ __HELP__ = """
 /commit
     Generate Funny Commit Messages
 
-
+/toimage
+    Ubah stiker ke foto.
+    
+/tosticker
+    Ubah foto ke stiker.
+    
 /id
     Get Chat_ID or User_ID
 
@@ -50,9 +64,6 @@ __HELP__ = """
 /carbon
     Make Carbon from code.
 
-/tts
-    Convert Text To Speech.
-
 /autocorrect [Reply to a message]
     Autocorrects the text in replied message.
 
@@ -68,13 +79,125 @@ __HELP__ = """
 /ping
     Check ping of all 5 DCs.
     
+/google [query] - Search using Google Search.
+(/tr, /trans, /translate) [lang code] - Translate text using Google Translate.
+
+/readqr [reply to photo] - Read QR Code From Photo.
+/createqr [text] - Convert Text to QR Code.
 
 """
 
 ASQ_LOCK = Lock()
 PING_LOCK = Lock()
 
+@app.on_message(filters.command(["tosticker"]))
+@capture_err
+async def tostick(client, message):
+    try:
+        if not message.reply_to_message or not message.reply_to_message.photo:
+            return await message.reply_text("Reply ke foto untuk mengubah ke sticker")
+        sticker = await client.download_media(
+            message.reply_to_message.photo.file_id,
+            f"tostick_{message.from_user.id}.webp",
+        )
+        await message.reply_sticker(sticker)
+        os.remove(sticker)
+    except Exception as e:
+        await message.reply_text(str(e))
 
+@app.on_message(filters.command(["toimage"]))
+@capture_err
+async def topho(client, message):
+    try:
+        if not message.reply_to_message or not message.reply_to_message.sticker:
+            return await message.reply_text("Reply ke sticker untuk mengubah ke foto")
+        if message.reply_to_message.sticker.is_animated:
+            return await message.reply_text("Ini sticker animasi, command ini hanya untuk sticker biasa.")
+        photo = await message.reply_to_message.download()
+        im = Image.open(photo).convert("RGB")
+        filename = f"toimg_{message.from_user.id}.png"
+        im.save(filename, "png")
+        await asyncio.gather(*[message.reply_document(filename), message.reply_photo(filename, caption=f"Sticker -> Image\n@{client.me.username}")])
+        os.remove(photo)
+        os.remove(filename)
+    except Exception as e:
+        await message.reply_text(str(e))
+        
+        
+@app.on_message(filters.command(["google"]))
+@capture_err
+async def gsearch(client, message):
+    if len(message.command) == 1:
+        return await message.reply("Give a query to search in Google!")
+    query = message.text.split(" ", maxsplit=1)[1]
+    msg = await message.reply_text(f"**Googling** for `{query}` ...")
+    try:
+        headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) " "Chrome/61.0.3163.100 Safari/537.36"}
+        html = await http.get(
+            f"https://www.google.com/search?q={query}&gl=id&hl=id&num=17",
+            headers=headers,
+        )
+        soup = BeautifulSoup(html.text, "lxml")
+
+        # collect data
+        data = []
+
+        for result in soup.select(".tF2Cxc"):
+            title = result.select_one(".DKV0Md").text
+            link = result.select_one(".yuRUbf a")["href"]
+            try:
+                snippet = result.select_one("#rso .lyLwlc").text
+            except:
+                snippet = "-"
+
+            # appending data to an array
+            data.append(
+                {
+                    "title": title,
+                    "link": link,
+                    "snippet": snippet,
+                }
+            )
+        arr = json.dumps(data, indent=2, ensure_ascii=False)
+        parse = json.loads(arr)
+        total = len(parse)
+        res = "".join(f"<a href='{i['link']}'>{i['title']}</a>\n{i['snippet']}\n\n" for i in parse)
+    except Exception:
+        exc = traceback.format_exc()
+        return await msg.edit(exc)
+    await msg.edit(
+        text=f"<b>Ada {total} Hasil Pencarian dari {query}:</b>\n{res}<b>Scraped by @{BOT_USERNAME}</b>",
+        disable_web_page_preview=True,
+    )
+
+@app.on_message(filters.command("readqr"))
+async def readqr(c, m):
+    if not m.reply_to_message:
+        return await m.reply("Please reply photo that contain valid QR Code.")
+    if not m.reply_to_message.photo:
+        return await m.reply("Please reply photo that contain valid QR Code.")
+    foto = await m.reply_to_message.download()
+    myfile = {"file": (foto, open(foto, "rb"), "application/octet-stream")}
+    url = "http://api.qrserver.com/v1/read-qr-code/"
+    r = await http.post(url, files=myfile)
+    os.remove(foto)
+    if res := r.json()[0]["symbol"][0]["data"] is None:
+        return await m.reply_msg(res)
+    await m.reply_msg(f"<b>QR Code Reader by @{c.me.username}:</b> <code>{r.json()[0]['symbol'][0]['data']}</code>", quote=True)
+
+
+@app.on_message(filters.command("createqr"))
+@capture_err
+async def makeqr(c, m):
+    if m.reply_to_message and m.reply_to_message.text:
+        teks = m.reply_to_message.text
+    elif len(m.command) > 1:
+        teks = m.text.split(None, 1)[1]
+    else:
+        return await m.reply("Please add text after command to convert text -> QR Code.")
+    url = f"https://api.qrserver.com/v1/create-qr-code/?data={quote(teks)}&size=300x300"
+    await m.reply_photo(url, caption=f"<b>QR Code Maker by @{c.me.username}</b>", quote=True)
+    
 @app2.on_message(SUDOERS & filters.command("ping", prefixes=USERBOT_PREFIX))
 @app.on_message(filters.command("ping"))
 async def ping_handler(_, message):
