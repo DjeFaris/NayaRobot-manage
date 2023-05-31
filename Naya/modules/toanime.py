ea59cd5de46e56142b9b87877448b810bdb104d3
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-05-31 18:51:50 +0700

    Create toanime.py

---
+++ Naya/modules/toanime.py
@@ -0,0 +1,50 @@
+# Copas Teriak Copas MONYET
+# Gay Teriak Gay Anjeng
+# @Rizzvbss | @Kenapanan
+# Kok Bacot
+# © @KynanSupport
+# FULL MONGO NIH JING FIX MULTI CLIENT
+# Credits : TomiX
+
+
+import os
+import asyncio
+import time
+from pyrogram import Client, filters, enums
+from pyrogram.types import *
+from pyrogram.raw.functions.messages import DeleteHistory
+from Naya import *
+
+__MODULE__ = "To Anime"
+__HELP__ = f"""
+/toanime [balas foto] - Ubah foto menjadi anime, wajah harus terlihat.
+"""
+
+@app.on_message(filters.command(["toanime"]))
+async def convert_image(_, message):
+    if not message.reply_to_message:
+        return await eor(message, text="**Mohon Balas Ke Foto**")
+    bot = "qq_neural_anime_bot"
+    if message.reply_to_message:
+        cot = await eor(message, text="**Processing...**")
+        await app2.unblock_user(bot)
+        ba = await message.reply_to_message.forward(bot)
+        await asyncio.sleep(30)
+        await ba.delete()
+        await cot.delete()
+        get_photo = []
+        async for ii in app2.search_messages(
+            bot, filter=enums.MessagesFilter.PHOTO
+        ):
+            if not ii.photo:
+                await message.reply(
+                    f"❌ {bot} Tidak dapat merespon permintaan ", quote=True
+                )
+            else:
+                get_photo.append(InputMediaPhoto(ii.photo.file_id))
+        await message.reply_photo(
+            photo=get_photo,
+            caption=f"<b>Maker by :{app.me.mention}</b>",
+        )
+        user_info = await app2.resolve_peer(bot)
+        return await app2.invoke(DeleteHistory(peer=user_info, max_id=0, revoke=True))
