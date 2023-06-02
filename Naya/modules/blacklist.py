11d3c65732651e510520ce3be0df7ff6542795b2
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-06-03 02:50:41 +0700

    update blacklist.py

--- Naya/modules/blacklist.py
+++ Naya/modules/blacklist.py
@@ -37,7 +37,7 @@ async def save_filters(_, message):
     kata = message.text.split(None, 1)[1].strip()
     if not kata:
         return await message.reply_text("**Usage**\n__/blacklist [balas pesan/berikan kata]__")
-    await message.reply_to_message.delete()
+#    await message.reply_to_message.delete()
     await message.delete()
     chat_id = message.chat.id
     await save_blacklist_filter(chat_id, kata)
