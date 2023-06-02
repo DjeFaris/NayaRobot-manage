4faf89eb0b0d6889d986558c1628e8b5f919bae0
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-06-02 07:59:56 +0700

    update toanime.py

--- Naya/modules/toanime.py
+++ Naya/modules/toanime.py
@@ -15,36 +15,121 @@ from pyrogram import Client, filters, enums
 from pyrogram.types import *
 from pyrogram.raw.functions.messages import DeleteHistory
 from Naya import *
+from Naya import app2 as client
 
 __MODULE__ = "To Anime"
 __HELP__ = f"""
 /toanime [balas foto] - Ubah foto menjadi anime, wajah harus terlihat.
 """
 
+
+async def dl_pic(client, download):
+    path = await client.download_media(download)
+    with open(path, "rb") as f:
+        content = f.read()
+    os.remove(path)
+    get_photo = BytesIO(content)
+    return get_photo
+
+
 @app.on_message(filters.command(["toanime"]))
-async def convert_image(_, message):
-    if not message.reply_to_message:
-        await eor(message, text="`Mohon Balas Ke Foto`")
-    else:
-        await eor(message, text="`Processing...`")
-    bot = "qq_neural_anime_bot"
+async def convert_anime(_, message):
+    Tm = await message.reply("<code>Processing...</code>")
     if message.reply_to_message:
-        cot = await eor(message, text="`Processing...`")
-        await app2.unblock_user(bot)
-        ba = await message.reply_to_message.copy(bot)
-        await asyncio.sleep(30)
-        get_photo = []
-        async for i in app2.search_messages(
-            bot, filter=enums.MessagesFilter.PHOTO, limit=1):
-                if not i.photo:
-                    await message.reply(f"❌ {bot} Tidak dapat merespon permintaan ",
-                    quote=True,
-                    )
-                else:
-                    get_photo.append(i)
-                    ajg = await app2.download_media(get_photo)
-                    await message.reply_photo(
-                        photo=ajg,
-                    )
-        user_info = await app2.resolve_peer(bot)
-        return await app2.invoke(DeleteHistory(peer=user_info, max_id=0, revoke=True))
+        if len(message.command) < 2:
+            if message.reply_to_message.photo:
+                file = "foto"
+                get_photo = message.reply_to_message.photo.file_id
+            elif message.reply_to_message.sticker:
+                file = "sticker"
+                get_photo = await dl_pic(client, message.reply_to_message)
+            elif message.reply_to_message.animation:
+                file = "gift"
+                get_photo = await dl_pic(client, message.reply_to_message)
+            else:
+                return await Tm.edit(
+                    "<code>Mohon balas ke foto</code>"
+                )
+        else:
+            if message.command[1] in ["foto", "profil", "photo"]:
+                chat = (
+                    message.reply_to_message.from_user
+                    or message.reply_to_message.sender_chat
+                )
+                file = "foto profil"
+                get = await client.get_chat(chat.id)
+                photo = get.photo.big_file_id
+                get_photo = await dl_pic(client, photo)
+    else:
+        if len(message.command) < 2:
+            return await Tm.edit(
+                "`Mohon balas ke foto...`"
+            )
+        else:
+            try:
+                file = "foto"
+                get = await client.get_chat(message.command[1])
+                photo = get.photo.big_file_id
+                get_photo = await dl_pic(client, photo)
+            except Exception as error:
+                return await Tm.edit(error)
+    await Tm.edit("<code>Processing...</code>")
+    await client.unblock_user("@qq_neural_anime_bot")
+    send_photo = await client.send_photo("@qq_neural_anime_bot", get_photo)
+    await asyncio.sleep(30)
+    await send_photo.delete()
+    await Tm.delete()
+    info = await client.resolve_peer("@qq_neural_anime_bot")
+    anime_photo = []
+    async for anime in client.search_messages(
+        "@qq_neural_anime_bot", filter=MessagesFilter.PHOTO
+    ):
+        anime_photo.append(
+            InputMediaPhoto(
+                anime.photo.file_id, caption=f"<b>Created by : {app.me.mention}</b>"
+            )
+        )
+    if anime_photo:
+        await app.send_media_group(
+            message.chat.id,
+            anime_photo,
+            reply_to_message_id=message.id,
+        )
+        return await client.invoke(DeleteHistory(peer=info, max_id=0, revoke=True))
+    else:
+        await app.send_message(
+            message.chat.id,
+            f"<code>Gagal merubah {file}</code>",
+            reply_to_message_id=message.id,
+        )
+        return await client.invoke(DeleteHistory(peer=info, max_id=0, revoke=True))
+
+@app.on_message(filters.command("efek"))
+async def convert_efek(_, message):
+    helo = get_arg(message)
+    rep = message.reply_to_message
+    if rep and helo:
+        tau = ["bengek", "robot", "jedug", "fast", "echo"]
+        if helo in tau:
+            Tm = await message.reply(f"`Processing, mengubah suara ke {helo}`")
+            indir = await client.download_media(rep)
+            KOMUT = {
+                "bengek": '-filter_complex "rubberband=pitch=1.5"',
+                "robot": "-filter_complex \"afftfilt=real='hypot(re,im)*sin(0)':imag='hypot(re,im)*cos(0)':win_size=512:overlap=0.75\"",
+                "jedug": '-filter_complex "acrusher=level_in=8:level_out=18:bits=8:mode=log:aa=1"',
+                "fast": "-filter_complex \"afftfilt=real='hypot(re,im)*cos((random(0)*2-1)*2*3.14)':imag='hypot(re,im)*sin((random(1)*2-1)*2*3.14)':win_size=128:overlap=0.8\"",
+                "echo": '-filter_complex "aecho=0.8:0.9:500|1000:0.2|0.1"',
+            }
+            ses = await asyncio.create_subprocess_shell(
+                f"ffmpeg -i '{indir}' {KOMUT[helo]} audio.mp3"
+            )
+            await ses.communicate()
+            await Tm.delete()
+            await rep.reply_voice("audio.mp3", caption=f"Efek {helo}")
+            os.remove("audio.mp3")
+        else:
+            await message.reply(f"`Silakan format yang anda inginkan : {tau}`")
+    else:
+        await Tm.edit(
+            f"`Silakan masukkan : <code>/efek bengek</code> balas ke ke audio atau mp3.`"
+        )
